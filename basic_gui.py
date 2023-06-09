c4dd663a8b99e1274b13732c4fc3bd398d7dbce8
tigerjoson <32243046+tigerjoson@users.noreply.github.com>
2023-06-09 17:08:15 +0800

    Add files via upload

---
+++ basic_gui.py
@@ -0,0 +1,15 @@
+
+#from tkinter import Tk, mainloop, TOP
+#
+import tkinter as tk
+window = tk.Tk()
+#set resolution
+# window.geometry('600x800')
+
+#The first parameter of the Label call is the name of the parent window
+#keyword parameter "text" specifies the text to be shown:
+#tk.Label(root, text="Hello Tkinter!")
+
+
+#system pause
+window.mainloop()
