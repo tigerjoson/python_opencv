d0f1879d93d545c745faa5edb34a6e75e2f5b5ff
tigerjoson <32243046+tigerjoson@users.noreply.github.com>
2023-06-10 20:00:09 +0800

    Update mybio.py

--- mybio.py
+++ mybio.py
@@ -1,22 +1,27 @@
 # ref:https://docs.opencv.org/4.x/d7/d4d/tutorial_py_thresholding.html
 # ref: https://python-forum.io/thread-25926.html
+# ref : https://pynative.com/python-list-files-in-a-directory/
 
 import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 import glob
-from tkinter import Tk
+import os as os
+import tkinter as tk
+from tkinter import filedialog
+
 # from tkinter import Tk for Python 3.x
-from tkinter.filedialog import askopenfilename
+#from tkinter.filedialog import askopenfilename
 
 
  # show an "Open" dialog box and return the path to the selected file
-filename = askopenfilename();
+#thefolder = tk.filedialog.askdirectory();
+thefullfilename =  tk.filedialog.askopenfilename();
+print(thefullfilename)
 
 # note: filename can not using chinese~!!
-cvpath = filename.replace("/","\\\\");
-print(cvpath);
-image = cv.imread(cvpath);
+
+image = cv.imread(thefullfilename);
 #cv.imshow('hi',image);
 
 
@@ -24,8 +29,8 @@ def plt_image(imagebgr):
     image_rgb = cv.cvtColor(output1, cv.COLOR_BGR2RGB)
     return image_rgb;
 
-if cv.haveImageReader(cvpath):
-    print(cv.haveImageReader(cvpath))
+if cv.haveImageReader(thefullfilename):
+    print(cv.haveImageReader(thefullfilename))
         
     ret, output1 = cv.threshold(image, 127, 255, cv.THRESH_BINARY_INV);
     plt.imshow(plt_image(output1));
