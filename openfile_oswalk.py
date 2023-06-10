e4e06f7cf05f41de151038ec8e9ad34d2a8141fa
tigerjoson <32243046+tigerjoson@users.noreply.github.com>
2023-06-10 21:34:18 +0800

    Update openfile_oswalk.py

--- openfile_oswalk.py
+++ openfile_oswalk.py
@@ -6,7 +6,17 @@ for root, dirs, files in os.walk("D:\\"):
     print(root, "consumes", end=" ")
     print(sum(getsize(join(root, name)) for name in files), end=" ")
     print("bytes in", len(files), "non-directory files")
+#???
+import os
+from os.path import join, getsize
+for a, b, c in os.walk("D:\\"):
+    print(a, "consumes", end=" ")
+    print(sum(getsize(join(a, name)) for name in c), end=" ")
+    print("bytes in", len(c), "non-directory c")
 
+    
+    
+    
     ###
 #ref: https://www.ewdna.com/2012/04/pythonoswalk.html
 # https://docs.python.org/3/library/os.html?highlight=walk#os.walk
