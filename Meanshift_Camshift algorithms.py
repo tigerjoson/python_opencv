99ef53fedce88eb43c41fed0ff2b2b4eb79d3b74
tigerjoson <32243046+tigerjoson@users.noreply.github.com>
2023-06-17 21:45:41 +0800

    Update Meanshift_Camshift algorithms.py

--- Meanshift_Camshift algorithms.py
+++ Meanshift_Camshift algorithms.py
@@ -22,7 +22,9 @@ cv.normalize(roi_hist,roi_hist,0,255,cv.NORM_MINMAX)
 # Setup the termination criteria, either 10 iteration or move by at least 1 pt
 term_crit = ( cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 1 )
 while(1):
-    ret, frame = cap.read()
+   #  why?????
+   # ret, frame = cap.read()
+    print(frame)
     if ret == True:
         hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
         dst = cv.calcBackProject([hsv],[0],roi_hist,[0,180],1)
