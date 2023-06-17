1077bae4820b2c97837d920dad6e5de9cab10e6e
tigerjoson <32243046+tigerjoson@users.noreply.github.com>
2023-06-18 01:17:19 +0800

    Add files via upload

---
+++ sh_info.py
@@ -0,0 +1,15 @@
+import os
+import sys
+
+
+print(sys.version);
+
+#at cmd not in python cmd
+#pip List
+
+#pause 
+#os.system("pause")
+
+dirname = os.path.dirname(sys.executable)
+
+print(dirname)
