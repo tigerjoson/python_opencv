b8ef7c3501e2f49d9d138d2ae5edb0be2a4a9a3c
tigerjoson <32243046+tigerjoson@users.noreply.github.com>
2023-06-18 14:04:33 +0800

    Add files via upload

---
+++ resize_pic.py
@@ -0,0 +1,18 @@
+import cv2 as cv;
+path =r'C:\\Users\\tiger\\Pictures\\test\\2.jpg';
+#the order must be right
+#must call namedWindow
+cvwindowname = "resize fig demo"
+cv.namedWindow(cvwindowname,cv.WINDOW_KEEPRATIO);
+#read picture return type = mat
+imagemat = cv.imread(path);
+
+cv.imshow(cvwindowname,imagemat);
+windowtuple = cv.getWindowImageRect(cvwindowname);
+xcoordinates = windowtuple[0];
+ycoordinates = windowtuple[1];
+width = windowtuple[2]
+height = windowtuple[3]
+#print(windowtuple)
+#print(type(cv.getWindowImageRect(cvwindowname)))
+cv.waitKey();
