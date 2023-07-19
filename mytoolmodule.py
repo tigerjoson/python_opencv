92cbdb9356584c3d87203cff7f41eef92d7c3ef0
tigerjoson <32243046+tigerjoson@users.noreply.github.com>
2023-07-19 16:18:36 +0800

    Update mytoolmodule.py

--- mytoolmodule.py
+++ mytoolmodule.py
@@ -4,7 +4,9 @@
 import cv2 as cv;
 import tkinter as tk;
 from tkinter import filedialog;
-
+import glob
+import os
+import pathlib
 #path = r"C:\Users\tiger\Pictures\Saved Pictures\2021-12-11 11 47 24.jpg"
 #the order must be right
 #must call namedWindow
@@ -40,5 +42,50 @@ class mycvwindowclass:
         ycoordinates = windowtuple[ 1];
         width = windowtuple[2]
         height = windowtuple[3]
+class myfileclass():
+    def __init__(self):
+        self._path = r"C:\\Users\\tiger\\Pictures\\test\\"  
+        self._extensionname=r"*jpg"
+        #print("default constuct=",glob.glob(self._path+self._extensionname))
+    def get_path(self):
+        return self._path
+    def set_path(self, foldername: str):
+        self._path=foldername
+    def set_p(self):
+        print("tk.filedialog.askdirectory();");
+        thefolder = tk.filedialog.askdirectory();
+        self._path = thefolder
+        print("self.get_path()=",self.get_path())
     
-       #cv.waitKey();
+    def set_ext(self, extensionname: str):
+        self._extensionname = extensionname;
+                
+    def get_ext(self):
+        return self._extensionname
+    def get_list(self):
+        searchstring = os.path.join(thefolder,extensionname);
+        print("searchstring=",searchstring)
+        allfiles = glob.glob(searchstring)
+        #print(allfiles);
+        
+    def getfilelist(self): 
+        thefolder = self.get_path();
+        #print ("thefolder=",thefolder)
+        extensionname = self.get_ext();
+        #print ("extensionname=",extensionname)
+        globresults= glob.glob(thefolder+"\\"+extensionname);
+       # print(type (globresults))
+       
+        return globresults
+    
+    def rename (self,fullfilename_src,newfilename:str):
+        filename = newfilename+os.path.splitext(os.path.basename(fullfilename_src))[0]
+        ext = pathlib.Path(fullfilename_src).suffix
+       # print(ext)
+        self._path = os.path.dirname(fullfilename_src)
+        #print(self.get_path()) ;
+        fullfilenewname = self.get_path()+"//"+filename+ext
+        print(fullfilenewname)
+        
+        #n = newfilename.join(filename)
+        #print(n)
